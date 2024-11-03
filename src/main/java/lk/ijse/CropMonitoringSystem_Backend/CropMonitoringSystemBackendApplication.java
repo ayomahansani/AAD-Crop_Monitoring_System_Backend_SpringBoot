89a506c3ed65ec95973eb0463a0dc5d58c4769f0
ayomahansani <ayohansi17@gmail.com>
2024-11-03 18:00:34 +0530

    add database and hibernate configurations

--- src/main/java/lk/ijse/CropMonitoringSystem_Backend/CropMonitoringSystemBackendApplication.java
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/CropMonitoringSystemBackendApplication.java
@@ -1,13 +1,25 @@
 package lk.ijse.CropMonitoringSystem_Backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableMethodSecurity
 public class CropMonitoringSystemBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CropMonitoringSystemBackendApplication.class, args);
+	}
+
+	//create modelmapper object
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
