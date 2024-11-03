3f15be1580905eadfa358c6deeaaa7c6fa888a70
ayomahansani <ayohansi17@gmail.com>
2024-11-03 15:48:08 +0530

    build gradle project

---
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/CropMonitoringSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.CropMonitoringSystem_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitoringSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropMonitoringSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/CropMonitoringSystem_Backend/CropMonitoringSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.CropMonitoringSystem_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
