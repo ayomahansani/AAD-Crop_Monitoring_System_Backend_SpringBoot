064d69c24cba6f56f4a0d14f60fd0b60814dcd26
ayomahansani <ayohansi17@gmail.com>
2024-11-08 20:36:05 +0530

    create Crop entity

---
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/entity/CropEntity.java
@@ -0,0 +1,29 @@
+package lk.ijse.CropMonitoringSystem_Backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+@Table(name = "Crop")
+public class CropEntity implements SuperEntity{
+
+    @Id
+    private String cropCode;
+    private String cropCommonName;
+    private String cropScientificName;
+    @Column(columnDefinition = "LONGTEXT")
+    private String cropImage;
+    private String category;
+    private String cropSeason;
+
+}
