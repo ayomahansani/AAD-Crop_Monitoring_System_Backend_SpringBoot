9c56b51b890c6aa1d888a6f9e2857c164d9552a9
ayomahansani <ayohansi17@gmail.com>
2024-11-08 21:29:05 +0530

    create Field entity

---
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/entity/FieldEntity.java
@@ -0,0 +1,32 @@
+package lk.ijse.CropMonitoringSystem_Backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.awt.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+@Table(name = "Field")
+public class FieldEntity implements SuperEntity{
+
+    @Id
+    private String fieldCode;
+    private String fieldName;
+    private Point fieldLocation;
+    private double extentsize;
+    @Column(columnDefinition = "LONGTEXT")
+    private String fieldImage1;
+    @Column(columnDefinition = "LONGTEXT")
+    private String fieldImage2;
+
+}
