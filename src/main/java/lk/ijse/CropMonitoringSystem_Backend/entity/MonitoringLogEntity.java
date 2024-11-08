38b092e4820c66d1cf0bf8bbf79471ed7976f575
ayomahansani <ayohansi17@gmail.com>
2024-11-08 20:24:46 +0530

    create MonitoringLog entity

--- src/main/java/lk/ijse/CropMonitoringSystem_Backend/entity/MonitoringLogEntity.java
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/entity/MonitoringLogEntity.java
@@ -1,4 +1,29 @@
 package lk.ijse.CropMonitoringSystem_Backend.entity;
 
-public class MonitoringLogEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+@Table(name = "Monitoring-Log")
+public class MonitoringLogEntity implements SuperEntity{
+
+    @Id
+    private String logCode;
+    private Date logDate;
+    private String logDetails;
+    @Column(columnDefinition = "LONGTEXT")
+    private String observedImage;
+
 }
