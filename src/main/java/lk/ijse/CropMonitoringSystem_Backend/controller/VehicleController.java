6536d0361feb74ad98f21a4741dc22231a46bbee
ayomahansani <ayohansi17@gmail.com>
2024-11-11 13:01:06 +0530

    create controller classes

---
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/controller/VehicleController.java
@@ -0,0 +1,11 @@
+package lk.ijse.CropMonitoringSystem_Backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/vehicles")
+@CrossOrigin
+public class VehicleController {
+}
