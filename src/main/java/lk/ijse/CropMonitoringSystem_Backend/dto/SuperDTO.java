07ca5bc5e170381e4e642a55c3de21b049114c98
ayomahansani <ayohansi17@gmail.com>
2024-11-11 11:48:53 +0530

    create dto classes and status interfaces

---
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/dto/SuperDTO.java
@@ -0,0 +1,6 @@
+package lk.ijse.CropMonitoringSystem_Backend.dto;
+
+import java.io.Serializable;
+
+public interface SuperDTO extends Serializable {
+}
---
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/dto/impl/VehicleDTO.java
@@ -0,0 +1,4 @@
+package lk.ijse.CropMonitoringSystem_Backend.dto.impl;
+
+public class VehicleDTO {
+}
