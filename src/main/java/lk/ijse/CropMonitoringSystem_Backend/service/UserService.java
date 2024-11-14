1ccafba746dedeecfae39c9bf182b13494292403
ayomahansani <ayohansi17@gmail.com>
2024-11-14 21:31:17 +0530

    add implementation to User entity

--- src/main/java/lk/ijse/CropMonitoringSystem_Backend/service/UserService.java
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/service/UserService.java
@@ -1,9 +1,13 @@
 package lk.ijse.CropMonitoringSystem_Backend.service;
 
+import lk.ijse.CropMonitoringSystem_Backend.dto.impl.UserDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Optional;
 
 public interface UserService {
 
+    Optional<UserDTO> findByEmail(String email);
     UserDetailsService userDetailsService(); // can fetch a user easily from the database
 
 }
--- src/main/java/lk/ijse/CropMonitoringSystem_Backend/service/impl/UserServiceIMPL.java
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/service/impl/UserServiceIMPL.java
@@ -1,13 +1,31 @@
 package lk.ijse.CropMonitoringSystem_Backend.service.impl;
 
+import lk.ijse.CropMonitoringSystem_Backend.dao.UserDAO;
+import lk.ijse.CropMonitoringSystem_Backend.dto.impl.UserDTO;
 import lk.ijse.CropMonitoringSystem_Backend.service.UserService;
+import lk.ijse.CropMonitoringSystem_Backend.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class UserServiceIMPL implements UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private Mapping mapping;
+
+
+    @Override
+    public Optional<UserDTO> findByEmail(String email) {
+        return Optional.empty();
+    }
 
     @Override
     public UserDetailsService userDetailsService() {
