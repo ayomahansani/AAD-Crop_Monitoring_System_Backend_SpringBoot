9551ee719c3665af3663562cd5b52336ff72904b
ayomahansani <ayohansi17@gmail.com>
2024-11-14 20:58:34 +0530

    implement JWTConfigFilter class

--- src/main/java/lk/ijse/CropMonitoringSystem_Backend/config/JWTConfigFilter.java
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/config/JWTConfigFilter.java
@@ -7,7 +7,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.CropMonitoringSystem_Backend.service.JWTService;
 import lk.ijse.CropMonitoringSystem_Backend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -16,11 +21,39 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JWTConfigFilter extends OncePerRequestFilter {
 
-    //private final JWTService jwtService;
+    private final JWTService jwtService;
     private final UserService userService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        String initToken = request.getHeader("Authorization");
+        String userEmail;
+        String extractedJwtToken;
+
+        //Validate the token
+        if(StringUtils.isEmpty(initToken) || !initToken.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        extractedJwtToken = initToken.substring(7); // substring the data next to the "Bearer " word
+        userEmail = jwtService.extractUserName(extractedJwtToken);
+
+
+        // user email check
+        if(StringUtils.isNotEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            var userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
+            if(jwtService.validateToken(extractedJwtToken, userDetails)) {
+                //add user to the security context
+                SecurityContext emptyContext = SecurityContextHolder.createEmptyContext();
+                var authToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                emptyContext.setAuthentication(authToken);
+                SecurityContextHolder.setContext(emptyContext);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
+
 }
--- src/main/java/lk/ijse/CropMonitoringSystem_Backend/service/impl/JWTServiceIMPL.java
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/service/impl/JWTServiceIMPL.java
@@ -1,4 +1,29 @@
 package lk.ijse.CropMonitoringSystem_Backend.service.impl;
 
-public class JWTServiceIMPL {
+import lk.ijse.CropMonitoringSystem_Backend.service.JWTService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTServiceIMPL implements JWTService {
+
+    @Override
+    public String extractUserName(String token) {
+        return "";
+    }
+
+    @Override
+    public String generateToken(UserDetails user) {
+        return "";
+    }
+
+    @Override
+    public boolean validateToken(String token, UserDetails user) {
+        return false;
+    }
+
+    @Override
+    public String refreshToken(UserDetails userDetails) {
+        return "";
+    }
 }
