92a1069a7c4dea64095f15b1362d7a0ebe9493f3
ayomahansani <ayohansi17@gmail.com>
2024-11-15 10:45:13 +0530

    implement SignIn class

--- src/main/java/lk/ijse/CropMonitoringSystem_Backend/secure/SignIn.java
+++ src/main/java/lk/ijse/CropMonitoringSystem_Backend/secure/SignIn.java
@@ -1,4 +1,15 @@
 package lk.ijse.CropMonitoringSystem_Backend.secure;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class SignIn {
+    private String email;
+    private String password;
 }
